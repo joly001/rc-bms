c81a846eaa891722f9e6d4f0196b9ee97df075aa
joly001 <joly_huang@163.com>
2019-04-09 01:02:52 +0800

    init

--- src/main/java/com/zcsoft/rc/bms/app/autoconfigure/components/ComponentsAutoconfigure.java
+++ src/main/java/com/zcsoft/rc/bms/app/autoconfigure/components/ComponentsAutoconfigure.java
@@ -1,10 +1,20 @@
 package com.zcsoft.rc.bms.app.autoconfigure.components;
 
+import com.sharingif.cube.security.web.exception.handler.validation.access.AccessDecisionCubeExceptionHandler;
+import com.sharingif.cube.web.exception.handler.WebCubeExceptionHandler;
+import com.sharingif.cube.web.exception.handler.validation.TokenValidationCubeExceptionHandler;
+import com.sharingif.cube.web.exception.handler.validation.ValidationCubeExceptionHandler;
+import com.sharingif.cube.web.springmvc.exception.NoHandlerFoundExceptionHandler;
+import com.zcsoft.rc.bms.app.exception.ExtendedSingleBindValidationCubeExceptionHandler;
+import com.zcsoft.rc.bms.app.exception.ExtendedSingleMethodArgumentNotValidExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class ComponentsAutoconfigure {
@@ -19,6 +29,29 @@ public class ComponentsAutoconfigure {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(source);
+    }
+
+    @Bean(name = "springMVCCubeExceptionHandlers")
+    public List<WebCubeExceptionHandler> createSpringMVCCubeExceptionHandlers(
+            AccessDecisionCubeExceptionHandler accessDecisionCubeExceptionHandler
+            , TokenValidationCubeExceptionHandler tokenValidationCubeExceptionHandler
+            , ExtendedSingleMethodArgumentNotValidExceptionHandler extendedSingleMethodArgumentNotValidExceptionHandler
+            , ExtendedSingleBindValidationCubeExceptionHandler extendedSingleBindValidationCubeExceptionHandler
+            , NoHandlerFoundExceptionHandler noHandlerFoundExceptionHandler
+            , ValidationCubeExceptionHandler validationCubeExceptionHandler
+            , WebCubeExceptionHandler webCubeExceptionHandler) {
+
+        List<WebCubeExceptionHandler> springMVCCubeExceptionHandlers = new ArrayList<WebCubeExceptionHandler>();
+        springMVCCubeExceptionHandlers.add(accessDecisionCubeExceptionHandler);
+        springMVCCubeExceptionHandlers.add(tokenValidationCubeExceptionHandler);
+        springMVCCubeExceptionHandlers.add(extendedSingleMethodArgumentNotValidExceptionHandler);
+        springMVCCubeExceptionHandlers.add(extendedSingleBindValidationCubeExceptionHandler);
+        springMVCCubeExceptionHandlers.add(noHandlerFoundExceptionHandler);
+        springMVCCubeExceptionHandlers.add(validationCubeExceptionHandler);
+        springMVCCubeExceptionHandlers.add(webCubeExceptionHandler);
+
+        return springMVCCubeExceptionHandlers;
+
     }
 
 }
